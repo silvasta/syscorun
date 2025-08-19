6eb2b5837eaf5ef04c5d4e91887d54ccac7476da
Silvan Stadelmann <silvasta3@gmail.com>
2025-08-19 13:51:24 +0200

    simulate lqr

--- src/run-simulation/simulate-lqr.py
+++ src/run-simulation/simulate-lqr.py
@@ -1,4 +1,5 @@
 from functools import partial
+import numpy as np
 from sysco import LQR
 
 from rich.console import Console
@@ -8,7 +9,7 @@ from rich.align import AlignMethod
 console = Console()
 
 
-def print_console(
+def print_cyan_panel(
     c: Console,
     subject: str,
     highligted: str,
@@ -27,13 +28,58 @@ def print_console(
 
 
 print_LQR_H1_sim = partial(
-    print_console,
+    print_cyan_panel,
     console,
     "LQR",
     "Simulation",
     subtitle=f"{LQR.__module__}.{LQR.__name__}",
 )
 
-HORIZON = 20
+print_step = partial(console.print, style="green")
+
+HORIZON = 30
+
 print_LQR_H1_sim(text=f"run for {HORIZON} steps")
 
+
+def apply_dynamics(A, x, B, u) -> np.ndarray:
+    A = np.atleast_2d(A)
+    x = np.atleast_2d(x).reshape(len(x), 1)
+    B = np.atleast_2d(B)
+    u = np.atleast_2d(u)
+    x_next = A @ x + B @ u
+    return x_next
+
+
+# system params
+A = [[1, 2], [3, 4]]
+B = [[2], [3]]
+# cost params
+Q = [[3, 0], [0, 4]]
+R = 1
+
+# initial condition
+x_0 = [2, 1]
+
+lqr = LQR(A, B, Q, R)
+x = np.atleast_2d(x_0).reshape(len(x_0), 1)
+# store the values
+x_trajcetory = []
+u_trajcetory = []
+cost_development = []
+
+for step in range(HORIZON):
+    print_step(f"start with iteration: {step}")
+    print(f"state: {x.flatten()}")
+    x_trajcetory += [x]
+    J = lqr.get_optimal_cost(x)
+    print(f"cost {J}")
+    cost_development += [J]
+    if step == HORIZON - 1:
+        break
+    u = lqr.get_input(x)
+    print(f"input: {u.flatten()}")
+    u_trajcetory += [u]
+    x = apply_dynamics(A, x, B, u)
+
+# print(x_trajcetory, cost_development, u_trajcetory, sep="\n\n")
